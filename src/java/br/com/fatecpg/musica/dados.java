4ce59f38d89aab455e72447643c06825b7049b76
Yan <yan.i.andrade@hotmail.com>
2019-09-11 12:19:44 -0300

    Segundo Commit - Alteração no dados.java
    alteração no arquivo dados.java

--- src/java/br/com/fatecpg/musica/dados.java
+++ src/java/br/com/fatecpg/musica/dados.java
@@ -10,5 +10,31 @@ package br.com.fatecpg.musica;
  * @author LeuoO
  */
 public class dados {
-    
+    private String musicas;
+    private String bandas;
+    private String discos;
+
+    public String getMusicas() {
+        return musicas;
+    }
+
+    public void setMusicas(String musicas) {
+        this.musicas = musicas;
+    }
+
+    public String getBandas() {
+        return bandas;
+    }
+
+    public void setBandas(String bandas) {
+        this.bandas = bandas;
+    }
+
+    public String getDiscos() {
+        return discos;
+    }
+
+    public void setDiscos(String discos) {
+        this.discos = discos;
+    }
 }
